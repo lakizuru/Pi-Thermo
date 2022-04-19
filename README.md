0c9b16ea35f2ee3d70d16bd3e806f3d3fab5fb20
Lakisuru Semasinghe <semasinghels@gmail.com>
2022-04-19 08:51:19 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Pi-Thermo
 Digital Thermometer made using Raspberry Pi 4B
+
+This repository contains the python code for a digital thermometer made using AdaFruit DHT11 temperature and humidity sensor with Raspberry Pi 4B and a servo motor.
+
+DHT11 python library was taken from https://github.com/binaryupdates/dht11-raspberrypi.git
+
+## Prerequisites
+- Raspberry Pi 4B
+- AdaFruit DHT11 Temperature and Humidity Sensor
+
+## Setup
+- Connect DHT11 sensor to GPIO 21
+- Connect Servo motor to GPIO 17
+
+*Disclaimer: This code was developed as a fulfillment for IoT for Big Data Analysis module in BSc. (Hons) in IT specializing in Data Science degree program of Sri Lanka Institute of Information Technology.*
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+# Importing  libraries
 import RPi.GPIO as GPIO
 import time
 import dht11
 
-servoPIN = 17
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
@@ -10,27 +10,36 @@ GPIO.setup(servoPIN, GPIO.OUT)
 # read data using Pin GPIO21 
 instance = dht11.DHT11(pin=21)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+servoPIN = 17 # Servo motor is connected to GPIO 17
+servo = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 
+servo.start(2.5) # Initialization servo motor to 2.5% duty cycle
+
+# Starting never ending loop
 while True:
-    result = instance.read()
+    result = instance.read() # Reading sensor input
     if result.is_valid():
         print("Temp: %d C" % result.temperature +' '+"Humid: %d %%" % result.humidity)
 
+    # Ignore if temperature is 0 as it is not practically valid
     if result.temperature == 0:
         continue
 
+    # Duty Cycle is calculated as follows
+    # 0 degrees angle = 2.5% duty cycle
+    # 90 degrees angle = 7.5% duty cycle
+    # 180 degrees angle = 12.5% duty cycle
+
     if result.temperature <= 15:
-        p.ChangeDutyCycle(5)
+        servo.ChangeDutyCycle(5)
         print("low")
 
     elif result.temperature > 15 and result.temperature <= 25:
-        p.ChangeDutyCycle(7.5)
+        servo.ChangeDutyCycle(7.5)
         print("mid")
 
     elif result.temperature > 25 and result.temperature:
-        p.ChangeDutyCycle(12.5)
+        servo.ChangeDutyCycle(12.5)
         print("high")
 
     time.sleep(0)
